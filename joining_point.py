cd622c3368b4927ce27f4ee6076a53bdfee8b2e1
Justin Thibault <justin.thibault@epitech.eu>
2022-05-22 14:34:19 +0200

    [ADD] adding joining point with brainstorming

---
+++ joining_point.py
@@ -0,0 +1,13 @@
+
+#recherche
+
+# si on fait une soustraction des resultats des deux séquences on trove 9 à chaque fois pour l'exemple du sujet
+# l'équation serait peut-être (pour l'exemple du sujet) 471 + x = y && 480 + x = y donc 471 + x = 480 + x
+# du coup trouver à chaque fois un resultat où la difference serait toujours 9 et vérifier à chaque fois si ces résultat non pas un JOIN POINT
+
+#########
+
+def main():
+    print ("recherche")
+
+main()
